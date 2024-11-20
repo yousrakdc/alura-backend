28cf7acca42d41d50ecbea82d982d8ca9599e772
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-11-20 15:54:49 +0100

    aula 3

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-const posts = [
-    {id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150"},
-    {id: 2, descricao: "Gato fofo dormindo", imagem: "https://placecats.com/millie/300/150"},
-    {id: 3, descricao: "Gato incrível", imagem: "https://placecats.com/millie/300/150"},
-];
-
+// Inicializando o servidor express
 const app = express();
-app.use(express.json())
+routes(app);
 
+// Iniciando o servidor para escutar na porta 3000
 app.listen(3000, () => {
     console.log("Servidor escutando ...");
-});
-
-app.get('/posts', (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-};
-
-app.get('/posts/:id', (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
 });
