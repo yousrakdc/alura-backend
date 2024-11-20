9177859fbcb9d5844666f0480934022b36b8a4b4
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-11-20 10:32:47 +0100

    criaçao de posts, id e mangoDB setup

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from "express";
 
+const posts = [
+    {id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150"},
+    {id: 2, descricao: "Gato fofo dormindo", imagem: "https://placecats.com/millie/300/150"},
+    {id: 3, descricao: "Gato incrível", imagem: "https://placecats.com/millie/300/150"},
+];
+
 const app = express();
+app.use(express.json())
+
 app.listen(3000, () => {
     console.log("Servidor escutando ...");
 });
 
-app.get('/api', (req, res) => {
-    res.status(200).send('Boas vindas');
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+};
+
+app.get('/posts/:id', (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
